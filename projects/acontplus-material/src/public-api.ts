59d7435b0fda02d8b46d9e032d86f3bb7a173360
Ivan Fernando Paz <ifer777@outlook.com>
2025-03-19 09:38:03 -0500

    update Angular dependencies to version 19.2.2 and increment package versions

--- projects/acontplus-material/src/public-api.ts
+++ projects/acontplus-material/src/public-api.ts
@@ -2,8 +2,4 @@
  * Public API Surface of acontplus-material
  */
 
-// export * from './lib/acontplus-material.service';
-// export * from './lib/acontplus-material.component';
-
-export * from './lib/components';
-export * from './lib/services';
+export * from './lib';
