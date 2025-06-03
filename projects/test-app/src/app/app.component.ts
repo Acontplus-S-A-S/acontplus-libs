da97b55baf0d44829798df67be837caf9fee4771
Ivan Fernando Paz <ifer777@outlook.com>
2025-06-03 00:41:19 -0500

    feat: add notification service integration and snackbar themes for improved user feedback

--- projects/test-app/src/app/app.component.ts
+++ projects/test-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApplicationService } from './application.service';
+import { NotificationService } from 'acontplus-utils';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,15 @@ export class AppComponent {
   title = 'test-app';
 
   private readonly _appService = inject(ApplicationService);
+  private readonly _notification = inject(NotificationService);
   ngOnInit() {
-    this._appService.get().subscribe((app) => {
-      console.log(app);
+    this._notification.show({
+      type: 'success',
+      message: 'New message received',
+      title: 'Notification',
     });
+    // this._appService.get().subscribe((app) => {
+    //   console.log(app);
+    // });
   }
 }
