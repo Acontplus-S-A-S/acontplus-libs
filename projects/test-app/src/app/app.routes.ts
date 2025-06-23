25fc74943ed709ce2752908df069a41f5089e90f
Ivan Fernando Paz <ifer777@outlook.com>
2025-06-23 12:47:16 -0500

    feat: update acontplus-ui-components to version 0.0.8 and enhance mat-dynamic-table component
    - Updated package-lock.json and package.json for acontplus-ui-components to version 0.0.8.
    - Improved mat-dynamic-table component with better template handling and dynamic row expansion.
    - Added new ProductComponent with dynamic table integration for product listing.
    - Implemented pagination and selection features in ProductComponent.
    - Created templates for actions and product images within the dynamic table.
    - Added unit tests for ProductComponent.
    - Updated routing to include new product page.

--- projects/test-app/src/app/app.routes.ts
+++ projects/test-app/src/app/app.routes.ts
@@ -6,4 +6,11 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./ui/pages/test/test.component').then((m) => m.TestComponent),
   },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./ui/pages/product/product.component').then(
+        (m) => m.ProductComponent
+      ),
+  },
 ];
